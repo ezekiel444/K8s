7be25c8c475c62f0530c43f10b59539007dde984
ezekiel444 <ezekielmatomi588@gmail.com>
2023-02-03 12:49:33 +0100

    working script added

--- ssl/Scriptwork.sh
+++ ssl/Scriptwork.sh
@@ -1,13 +1,13 @@
 echo "subjectAltName=DNS:ec2-3-253-104-57.eu-west-1.compute.amazonaws.com,IP:3.253.104.57" > server-ext.cnf
 
 
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=FR/ST=Occitanie/L=Toulouse/O=Tech School/OU=Education/CN=*.techschool.guru/emailAddress=matomiezekiel6@gmail.com"
+openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca-key.pem -out ca-cert.pem -subj "/C=FR/ST=Occitanie/L=Lyon/O=Matomi Lucky Ezekiel/OU=Education/CN=Matomi/emailAddress=matomiezekiel6@gmail.com"
 
 
 openssl x509 -in ca-cert.pem -noout -text
 
 
-openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=FR/ST=Ile de France/L=Paris/O=PC Book/OU=Computer/CN=*.pcbook.com/emailAddress=matomiezekiel6@gmail.com"
+openssl req -newkey rsa:4096 -nodes -keyout server-key.pem -out server-req.pem -subj "/C=FR/ST=Ile de France/L=Paris/O=PC Book/OU=Computer/CN=Matomi/emailAddress=matomiezekiel6@gmail.com"
 
 openssl x509 -req -in server-req.pem -days 60 -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -extfile server-ext.cnf
 
